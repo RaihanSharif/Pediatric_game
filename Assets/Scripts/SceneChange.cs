281d063607ec8e6d6290678dc5c29e4bb95bcf2c
Raihan Sharif <raihansharif92@gmail.com>
2018-02-12 15:30:21 +0000

    Add transition from Splash to main menu

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public int num;
+	
+	//<param name="abcd>comment about param</param>
+	/// <summary>
+	/// On click transitions from start scence to menu scene
+
+	/// </summary>
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(num);
+            Debug.Log("Scene changed to " + num);
+        }
+    }
+}
