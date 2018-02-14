d176dcdbb8d6d19a848a3bff44b0644df736105a
Daniel Mannix <daniel.mannix@kcl.ac.uk>
2018-02-14 18:17:46 +0000

    Began Work On Adding Text
    Text element added to panel,
    Test Text File added to populate text element

---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImporter : MonoBehaviour {
+
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+	}
+}
