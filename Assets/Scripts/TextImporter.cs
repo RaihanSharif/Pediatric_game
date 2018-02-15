bd71485fbbea44fd9b421afa3120ad528afdc94b
Daniel Mannix <daniel.mannix@kcl.ac.uk>
2018-02-15 15:08:27 +0000

    Successfully Implement Text Scrolling
    Using a button with an onclick event, the dialog changes through a supplied array(via text file, seperated by \n)
    Added inline and method comments

--- Assets/Scripts/TextImporter.cs
+++ Assets/Scripts/TextImporter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextImporter : MonoBehaviour {
 
@@ -8,12 +9,47 @@ public class TextImporter : MonoBehaviour {
     public TextAsset textFile;
     public string[] textLines;
 
+    private int currentLine = 0;
+    private int endLine = -1;
+
+    private Text TextBox;
+    private Button nextDialogButton;
+
 	// Update is called once per frame
-	void Update () {
+	void Start () {
 		
+        //Check for a valid text file
         if(textFile != null)
         {
+            //Create an array of dialog strings from a supplied newline-seperated text file
             textLines = (textFile.text.Split('\n'));
+            endLine = textLines.Length;
+
+            //Assign TextBox to the text component of this game object and assign nextDialogButton to a child button and give it a on click action listener
+            TextBox = this.gameObject.GetComponent<Text>();
+            nextDialogButton = this.gameObject.GetComponentInChildren<Button>();
+            nextDialogButton.onClick.AddListener(nextText);
         }
-	}
+
+    }
+
+    /***Increments Currentline, thusly scrolling through a supplied dialog text file
+     */
+    void nextText()
+    {
+        currentLine++;
+        if (currentLine >= endLine && endLine != -1)
+        {
+            currentLine = 0;
+        }
+    }
+
+    void Update()
+    {
+        //Check and assign text on every frame refresh
+        if(endLine != -1 && TextBox != null && textFile != null)
+        {
+            TextBox.text = textLines[currentLine];
+        }
+    }
 }
