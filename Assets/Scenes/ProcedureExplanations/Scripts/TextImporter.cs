c285880ed2450541fb195f3df4462d068003aa3c
Daniel Mannix <daniel.mannix@kcl.ac.uk>
2018-02-22 15:43:25 +0000

    Create Procedure Explanation Scene
    Additions to file structure as necessary,
    Adjusted text for explanation (procedure specific)

--- Assets/Scenes/ProcedureExplanations/Scripts/TextImporter.cs
+++ Assets/Scenes/ProcedureExplanations/Scripts/TextImporter.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class TextImporter : MonoBehaviour {
 
 
-    public TextAsset textFile;
+    private TextAsset textFile;
     public string[] textLines;
+    public TextAsset babyInjectionFile;
+    public TextAsset kidInjectionFile;
+    public TextAsset kidIngestionFile;
+    public TextAsset babyIngestionFile;
 
     private int currentLine = 0;
     private int endLine = -1;
@@ -16,10 +20,37 @@ public class TextImporter : MonoBehaviour {
     private Button nextDialogButton;
     private GameObject dialogBox;
 
+    public int procedureSelected = 0; //0 = Failed selection, 1 = Baby Injection, 2 = Kid injection, 3 = baby ingestion, 4 = KID INGESTION
+
 	// Update is called once per frame
 	void Start () {
-		
+
         //Check for a valid text file
+
+        switch (procedureSelected)
+        {
+            case 0:
+                textFile = null;
+                break;
+
+            case 1:
+                textFile = babyInjectionFile;
+                break;
+            case 2:
+
+                textFile = kidInjectionFile;
+                break;
+            case 3:
+
+                textFile = babyIngestionFile;
+                break;
+
+            case 4:
+                textFile = kidIngestionFile;
+                break;
+
+
+        }
         if(textFile != null)
         {
             //Create an array of dialog strings from a supplied newline-seperated text file
