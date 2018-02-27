c89729aa919050d63a4a49cef2c01aad0ceef0a8
REKLV <rewaz96@hotmail.com>
2018-02-27 17:44:20 +0000

    Reception window dialogue and reception room
    Things done:
    We have 2 scenes. When the user clicks on the reception window in the reception room scene, the second scene is displayed where a nurse is displayed welcoming the user and instructing him of what do next ( E.g. click on the ingestion room to get food ).
    
    Possible improvement:
    The dialogue becomes dynamic depending on the game settings that has been chosen from the beginning of the game ( ingestion/injection and age ).

---
+++ Assets/windowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class windowScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("well done u pressed me");
+        SceneManager.LoadScene(1);
+    }
+
+}
