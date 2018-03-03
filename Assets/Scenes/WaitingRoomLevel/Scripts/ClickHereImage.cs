3c875b196162850c7a9a1ca8d6a1481fd06ffec8
abdurmasood <abdurrafeym@gmail.com>
2018-03-03 14:23:56 +0000

    git not working

---
+++ Assets/Scenes/WaitingRoomLevel/Scripts/ClickHereImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+    //This class needs implementation to be done for animation
+public class NewBehaviourScript : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+       
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void ChangeSizeOfImageWhenClicked()
+    {
+        //clickHere.sizeDelta = new Vector2(100, 100);
+    }
+}
