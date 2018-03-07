89cc87e44472ba86199e5882c352309548d7ccd2
Daniel Lopes de Castro <danieldecastro98@hotmail.com>
2018-03-07 15:57:27 +0000

    Update paper toss
    Rescale game objects and implement score counter text.

--- Assets/Scenes/WaitingRoomLevel/Scripts/ScoreCounter.cs
+++ Assets/Scenes/WaitingRoomLevel/Scripts/ScoreCounter.cs
@@ -1,29 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour {
 
 	public BoxCollider2D scoreIncrementCollider;
-	public int score = 0;
-	
-	// Update is called once per frame
-	void Update () {
+	public int score;
+	public Text scoreText;
+
+	void Start(){
+		score = 0;
+		scoreText.text = "WAITING ROOM";
+	}
+
+	void Update(){
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		
 		score++;
+		scoreText.text = "Score: " + score.ToString();
 		Wait();
-		other.gameObject.transform.Translate(new Vector3((2.6f - transform.position.x), (-transform.position.y)));
-
-		Debug.Log(score);
+		other.gameObject.transform.Translate(new Vector3(550f, transform.position.y-200f), Space.World); //instead of 550f you could use a randomiser between -550 and 550.
 			
 	}
 
 	IEnumerator Wait(){
-		Debug.Log("AAaaaaaaaaah!");
 		yield return new WaitForSeconds(3);
-		Debug.Log("Ddfblkbbdfblbfadjhvdsjkhv!");
 	}
+
 }
