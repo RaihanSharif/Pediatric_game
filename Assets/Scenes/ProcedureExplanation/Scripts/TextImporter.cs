9dd5200d861b37f851edfc2bea77287ccad853ab
Alex <a.pessina@yandex.ru>
2018-03-07 22:15:34 +0000

    Create Pause menu prefab
    -Change pause menu canvas properties to fit all scenes
    - Make prefab from pause menu
    -One-step drag'n'drop import of pause menu

--- Assets/Scenes/ProcedureExplanation/Scripts/TextImporter.cs
+++ Assets/Scenes/ProcedureExplanation/Scripts/TextImporter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextImporter : MonoBehaviour {
+public class TextImporterCopy : MonoBehaviour {
 
 
     private TextAsset textFile;
