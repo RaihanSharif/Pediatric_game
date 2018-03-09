6569fec305247b183cb124383d682441f499fdd4
Amedeo China <amedeo.china@kcl.ac.uk>
2018-03-09 13:45:41 +0000

    Complete integration tests for MainMenu.

--- Assets/Scenes/MainMenu/Tests/IntegrationTests/MainMenuIntegrationTest.cs
+++ Assets/Scenes/MainMenu/Tests/IntegrationTests/MainMenuIntegrationTest.cs
@@ -8,7 +8,7 @@ public class MainMenuIntegrationTest {
 
 	/// <summary>
 	/// Checking clicking on two buttons : 
-	/// 0 to 5 years & Ingestion, leads
+	/// 0 to 5 years then Ingestion, leads
 	/// to the loading of the Baby Injection Scene 
 	/// </summary>
 	/// <returns>The menu moves to ingestion baby passed.</returns>
@@ -23,8 +23,23 @@ public class MainMenuIntegrationTest {
 
 	/// <summary>
 	/// Checking clicking on two buttons : 
-	/// 0 to 5 years & Ingestion, leads
+	/// Ingestion then 0 to 5 years, leads
 	/// to the loading of the Baby Injection Scene 
+	/// </summary>
+	/// <returns>The menu moves to ingestion baby passed.</returns>
+	[UnityTest]
+	public IEnumerator MainMenuMovesToInggestionBabyPassed() {
+		var manager = new GameObject().AddComponent<EventManager>();
+		manager.ingestionButtonPressed ();
+		manager.babyButtonPressed ();
+		Assert.AreEqual(manager.chosenScene, "IngestionBaby");
+		yield return null;
+	}
+
+	/// <summary>
+	/// Checking clicking on two buttons : 
+	/// 6 to 10 years then Ingestion, leads
+	/// to the loading of the Kid Ingestion Scene 
 	/// </summary>
 	/// <returns>The menu moves to ingestion baby passed.</returns>
 	[UnityTest]
@@ -38,8 +53,23 @@ public class MainMenuIntegrationTest {
 
 	/// <summary>
 	/// Checking clicking on two buttons : 
-	/// 6 to 10 years & Injection, leads
-	/// to the loading of the Baby Injection Scene 
+	/// Ingestion then 6 to 10 years, leads
+	/// to the loading of the Kid Ingestion Scene 
+	/// </summary>
+	/// <returns>The menu moves to ingestion baby passed.</returns>
+	[UnityTest]
+	public IEnumerator MainMenuMovesToInggestionKidPassed() {
+		var manager = new GameObject().AddComponent<EventManager>();
+		manager.ingestionButtonPressed ();
+		manager.kidButtonPressed ();
+		Assert.AreEqual(manager.chosenScene, "IngestionKid");
+		yield return null;
+	}
+
+	/// <summary>
+	/// Checking clicking on two buttons : 
+	/// 6 to 10 years then Injection, leads
+	/// to the loading of the Kid Injection Scene 
 	/// </summary>
 	/// <returns>The menu moves to ingestion baby passed.</returns>
 	[UnityTest]
@@ -47,6 +77,21 @@ public class MainMenuIntegrationTest {
 		var manager = new GameObject().AddComponent<EventManager>();
 		manager.babyButtonPressed ();
 		manager.injectionButtonPressed ();
+		Assert.AreEqual(manager.chosenScene, "InjectionKid");
+		yield return null;
+	}
+
+	/// <summary>
+	/// Checking clicking on two buttons : 
+	/// Injection then 6 to 10 years, leads
+	/// to the loading of the Kid Injection Scene 
+	/// </summary>
+	/// <returns>The menu moves to ingestion baby passed.</returns>
+	[UnityTest]
+	public IEnumerator MainMenuMovesToInjjectionBabyPassed() {
+		var manager = new GameObject().AddComponent<EventManager>();
+		manager.injectionButtonPressed ();
+		manager.babyButtonPressed ();
 		Assert.AreEqual(manager.chosenScene, "InjectionKid");
 		yield return null;
 	}
