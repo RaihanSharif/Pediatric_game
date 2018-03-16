42d065511177bc950df76ef4110cec7c0c0c27f9
Amedeo China <amedeo.china@kcl.ac.uk>
2018-03-16 16:17:38 +0000

    Complete part of integration test for Kid Ingestion scene.

--- Assets/Scenes/IngestionKid/Tests/IntegrationTests/IngKidIntegrationTest.cs
+++ Assets/Scenes/IngestionKid/Tests/IntegrationTests/IngKidIntegrationTest.cs
@@ -7,11 +7,39 @@ using UnityEngine.UI;
 
 public class IngKidIntegrationTest {
 
-	// A UnityTest behaves like a coroutine in PlayMode
-	// and allows you to yield null to skip a frame in EditMode
+	private void LoadSceneByName(string name){
+
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
 	[UnityTest]
 	public IEnumerator IngKidIntegrationTestWithEnumeratorPasses() {
-		
+		LoadSceneByName ("IngestionKid");
 		yield return null;
+
+		var spoon = GameObject.Find ("spoon");
+		for (int i = 0; i < 24; i++) {
+			spoon.transform.position = new Vector3 (-3.87f, 0.37f, 0f);
+			yield return new WaitForSeconds (0.1f);
+			spoon.transform.position = new Vector3 (4.9f, 0.001f, 0f);
+			yield return new WaitForSeconds (0.1f);
+		}
+
+//		var food = GameObject.Find ("food");
+//		var script = food.GetComponent<changeFoodOnContact> ();
+//		yield return new WaitForSeconds (1);
+//		yield return null;
+
+//		var button = GameObject.FindGameObjectWithTag ("NextLvlButton").GetComponent<Button>();
+//		button.onClick.Invoke ();
+//		yield return null;
+
+		//not this
+		Assert.AreEqual(1,1);
+		//check whether we have changed scene as a result
+//		Assert.AreEqual ("WaitingRoom", SceneManager.GetActiveScene().name);
 	}
 }
