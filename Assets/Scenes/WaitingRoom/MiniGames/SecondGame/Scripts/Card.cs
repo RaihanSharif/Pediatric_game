e60b3c5afd322bc2a87c004c6d83c9d73154bf06
REKLV <rewaz96@hotmail.com>
2018-03-17 19:24:15 +0000

    Finished game logic & images of cards left
    Finished the logic of the game using 3 classes MenuBehaviour, Card, GameManager.
    The images for each card should be changed to the staffs images as we discussed in the meeting.

---
+++ Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/Card.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class Card : MonoBehaviour
+{
+    // holts all fliping cards process 
+    public static bool DO_NOT = false;
+    [SerializeField]
+    private int _state;
+    [SerializeField]
+    private int _cardValue;
+    [SerializeField]
+    private bool _initialized = false;
+
+    private Sprite _cardBack;
+    private Sprite _cardFace;
+
+    private GameObject _manager;
+
+    void Start()
+    {
+        _state = 1;
+        _manager = GameObject.FindGameObjectWithTag("Manager");
+    }
+
+    /// <summary>
+    /// initialising all values
+    /// </summary>
+    public void setupGraphics()
+    {
+        _cardBack = _manager.GetComponent<GameManager>().getCardBack();
+        _cardFace = _manager.GetComponent<GameManager>().getCardFace(_cardValue);
+        flipCard();
+    }
+
+    public void flipCard()
+    {
+
+        if (_state == 0)
+            _state = 1;
+        else if (_state == 1)
+            _state = 0;
+
+        // if state = 0 ( means card is faced up ) then flip it back
+        if (_state == 0 && !DO_NOT) GetComponent<Image>().sprite = _cardBack;
+        // if state 1 ( means car is faced down ) then flip it up
+        else if (_state == 1 && !DO_NOT) GetComponent<Image>().sprite = _cardFace;
+    }
+
+    public int cardValue
+    {
+        get { return _cardValue; }
+        set { _cardValue = value; }
+    }
+
+    public int state
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    public bool initialized
+    {
+        get { return _initialized; }
+        set { _initialized = value; }
+    }
+
+    public void falseCheck()
+    {
+        StartCoroutine(pause());
+    }
+
+    // gives a little buffer when the cards is flipped over and check if it is a match and how long the card should wait until flipped again
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if (_state == 0) GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1) GetComponent<Image>().sprite = _cardFace;
+        DO_NOT = false;
+    }
+}
---
+++ Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] cardFace;
+    public Sprite cardBack;
+    public GameObject[] cards;
+    public Text matchText;
+
+    private bool _init = false;
+    private int _matches = 6;
+
+    void Update()
+    {
+        if (!_init) { initializeCards(); }
+        if (Input.GetMouseButtonUp(0)) checkCards();
+    }
+
+    void initializeCards()
+    {
+        // every card has a match 
+        for (int id = 0; id < 2; id++)
+        {
+            for (int i = 1; i < 7 ; i++)
+            {
+                bool test = false;
+                int choice = 0;
+                // tests if cards is initialised 
+                while (!test)
+                {
+                    choice = Random.Range(0, cards.Length);
+                    test = !(cards[choice].GetComponent<Card>().initialized);
+                }
+                // it initialises it 
+                cards[choice].GetComponent<Card>().cardValue = i;
+                cards[choice].GetComponent<Card>().initialized = true;
+            }
+        }
+
+        foreach (GameObject c in cards)
+            c.GetComponent<Card>().setupGraphics();
+        if (!_init) _init = true;
+    }
+
+    public Sprite getCardBack()
+    {
+        return cardBack;
+    }
+
+    public Sprite getCardFace(int i)
+    {
+        return cardFace[i - 1];
+    }
+
+    void checkCards()
+    {
+        List<int> c = new List<int>();
+        for (int i = 0; i < cards.Length; i++)
+        {
+            // going through the list and if 1 card up then add to list
+            if (cards[i].GetComponent<Card>().state == 1)
+                c.Add(i);
+        }
+
+        // if 2 cards up then compare the cards
+        if (c.Count == 2)
+            cardComparison(c);
+    }
+
+    void cardComparison(List<int> c)
+    {
+        Card.DO_NOT = true;
+        int x = 0;
+        if (cards[c[0]].GetComponent<Card>().cardValue == cards[c[1]].GetComponent<Card>().cardValue)
+        {
+            x = 2;
+            _matches--;
+            matchText.text = "Number of Matches: " + _matches;
+            if (_matches == 0)
+                SceneManager.LoadScene("SecondGameMenu");
+        }
+        for (int i = 0; i < c.Count; i++)
+        { cards[c[i]].GetComponent<Card>().state = x;
+            cards[c[i]].GetComponent<Card>().falseCheck();
+        }
+    }
+}
