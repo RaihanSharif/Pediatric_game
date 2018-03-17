2624efaeab866620ec3611b8502f718173e804ca
Alex <a.pessina@yandex.ru>
2018-03-17 15:21:04 +0000

    Create generic version of popup dialogues
    Move generic version to shared scripts
    Delete old script copies in scene folders

--- Assets/Shared Scripts/PopupTextImporter.cs
+++ Assets/Shared Scripts/PopupTextImporter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CameraTextImporter : GenericTextImporter {
+public class PopupTextImporter : GenericTextImporter {
 
 	void Start () {
 
