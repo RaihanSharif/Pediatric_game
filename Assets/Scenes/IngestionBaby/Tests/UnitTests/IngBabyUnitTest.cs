34d79a7425864f57d6201f90a439a2ba4699bff4
AmadNinja <amedeo.china@kcl.ac.uk>
2018-03-23 18:15:06 +0000

    Finish unit testing on BabyIngestion scene

--- Assets/Scenes/IngestionBaby/Tests/UnitTests/IngBabyUnitTest.cs
+++ Assets/Scenes/IngestionBaby/Tests/UnitTests/IngBabyUnitTest.cs
@@ -233,4 +233,118 @@ public class IngBabyUnitTest {
 
 	}
 
+	/// <summary>
+	/// Es the third blue bubble popped passes.
+	/// </summary>
+	[UnityTest]
+	public IEnumerator H_BottleDiminshesByHalfPasses() {
+		LoadSceneByName ("IngestionBaby");
+		yield return null;
+
+		yield return new WaitForSeconds (3);
+		var bubble = GameObject.Find ("BlueBubble1").GetComponent<BubbleClick> ();
+		bubble.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+		var otherBubble = GameObject.Find ("PinkBubble1").GetComponent<BubbleClick> ();
+		otherBubble.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+		otherBubble.OnMouseDown ();
+		yield return new WaitForSeconds (1);
+
+		var secondBlue = GameObject.Find ("BlueBubble2").GetComponent<BubbleClick> ();
+		secondBlue.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var thirdBlue = GameObject.Find ("BlueBubble3").GetComponent<BubbleClick> ();
+		thirdBlue.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var secondPurple = GameObject.Find ("PinkBubble2").GetComponent<BubbleClick> ();
+		secondPurple.OnMouseDown ();
+		secondPurple.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var thirdPurple = GameObject.Find ("PinkBubble3").GetComponent<BubbleClick> ();
+		thirdPurple.OnMouseDown ();
+		thirdPurple.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var bottle = GameObject.Find ("babyBottle").GetComponent<playAnimationOnClick> ();
+		bottle.OnMouseDown ();
+		var anim = GameObject.Find ("babyBottle").GetComponent<Animator> ();
+		anim.Play ("BabyBottleAnim");
+		var bottleScript = GameObject.Find ("babyBottle").GetComponent<Babybottle> ();
+		bottleScript.OnMouseDown ();
+		yield return null;
+		yield return new WaitForSeconds (0.4f);
+
+		for (int i = 0; i < 2; i++) {
+			bottle.OnMouseDown ();
+			anim.Play ("BabyBottleAnim");
+			bottleScript.OnMouseDown ();
+			yield return null;
+			yield return new WaitForSeconds (0.4f);
+		}
+
+		Assert.AreEqual (3, bottleScript.ctr);
+
+	}
+
+	/// <summary>
+	/// Es the third blue bubble popped passes.
+	/// </summary>
+	[UnityTest]
+	public IEnumerator I_BottleEmptiedPasses() {
+		LoadSceneByName ("IngestionBaby");
+		yield return null;
+
+		yield return new WaitForSeconds (3);
+		var bubble = GameObject.Find ("BlueBubble1").GetComponent<BubbleClick> ();
+		bubble.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+		var otherBubble = GameObject.Find ("PinkBubble1").GetComponent<BubbleClick> ();
+		otherBubble.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+		otherBubble.OnMouseDown ();
+		yield return new WaitForSeconds (1);
+
+		var secondBlue = GameObject.Find ("BlueBubble2").GetComponent<BubbleClick> ();
+		secondBlue.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var thirdBlue = GameObject.Find ("BlueBubble3").GetComponent<BubbleClick> ();
+		thirdBlue.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var secondPurple = GameObject.Find ("PinkBubble2").GetComponent<BubbleClick> ();
+		secondPurple.OnMouseDown ();
+		secondPurple.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var thirdPurple = GameObject.Find ("PinkBubble3").GetComponent<BubbleClick> ();
+		thirdPurple.OnMouseDown ();
+		thirdPurple.OnMouseDown ();
+		yield return new WaitForSeconds (2);
+
+		var bottle = GameObject.Find ("babyBottle").GetComponent<playAnimationOnClick> ();
+		bottle.OnMouseDown ();
+		var anim = GameObject.Find ("babyBottle").GetComponent<Animator> ();
+		anim.Play ("BabyBottleAnim");
+		var bottleScript = GameObject.Find ("babyBottle").GetComponent<Babybottle> ();
+		bottleScript.OnMouseDown ();
+		yield return null;
+		yield return new WaitForSeconds (0.4f);
+
+		for (int i = 0; i < 5; i++) {
+			bottle.OnMouseDown ();
+			anim.Play ("BabyBottleAnim");
+			bottleScript.OnMouseDown ();
+			yield return null;
+			yield return new WaitForSeconds (0.4f);
+		}
+
+		Assert.AreEqual (true, bottleScript.gameWon);
+
+	}
+
 }
