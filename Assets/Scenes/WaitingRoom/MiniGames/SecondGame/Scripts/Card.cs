5ec391d40c62d7267d058a6dc49d9975e74ca0d4
REKLV <rewaz96@hotmail.com>
2018-03-24 03:58:21 +0000

    Fixed 2 bugs, refacotred and commented the code
    fixed the bug so that now when the cards are facing up then the card will be disabled ( it will become unclickable ) and when faced back then it will be enabled again. and the card that have been matched will be disabled as well as those two bugs caused problem for the tutorial. unused code is deleted and the whole classes are commented out so that it is ready to be tested.

--- Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/Card.cs
+++ Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/Card.cs
@@ -7,9 +7,13 @@ public class Card : MonoBehaviour
     // holts all fliping cards process 
     public static bool DO_NOT = false;
     [SerializeField]
+    // if state = 1 then card faces down and if state = 0 then card faces up
     private int _state;
+    // each pair has the same card value, so as there are 16 cards then the max for the card value will be 8
     [SerializeField]
     private int _cardValue;
+
+    // boolean flag to check if the cards are initialised or not 
     [SerializeField]
     private bool _initialized = false;
 
@@ -21,12 +25,6 @@ public class Card : MonoBehaviour
     [SerializeField]
     private GameManager gameManagerScript;
 
-    private bool firstCardClicked;
-    private bool firstCardClickedSecondTime;
-
-    private bool playedAnimationForCorrect;
-    private bool runnedOnce;
-
     Animator animator;
 
     AudioSource sound;
@@ -34,8 +32,23 @@ public class Card : MonoBehaviour
     public AudioClip winning;
     public AudioClip losing;
 
+    // this counts how many times the first card is being pressed ( for tutorial ). So when it is pressed once then 
+    // the worng match will be animated and when it is being pressed for the second time then the right match will be animated
     public int numOfClicksForFirstCard = 0;
 
+    /// <summary>
+    /// always checks if cards needs to be disabled or enabled based on the state of the card 
+    /// disable when card faced up
+    /// enable when card faced down
+    /// </summary>
+    void Update()
+    {
+        disableAndEnableCard();
+    }
+
+    /// <summary>
+    /// At the start of the scene initialise the variables for sound, state, manager and animator
+    /// </summary>
     void Start()
     {
         sound = GetComponent<AudioSource>();
@@ -45,7 +58,8 @@ public class Card : MonoBehaviour
     }
 
     /// <summary>
-    /// initialising all values
+    /// sets the vale of the card back and the card face. The card face depends on the value of the card. 
+    /// cards with the same value will hav ethe same cardFace ( have the same sprite ) 
     /// </summary>
     public void setupGraphics()
     {
@@ -54,6 +68,9 @@ public class Card : MonoBehaviour
         flipCard();
     }
 
+    /// <summary>
+    /// flips the cards so when the state is 0 it dispalys the cardFace sprite and when the state is 1 then it displays the cardBack sprite
+    /// </summary>
     public void flipCard()
     {
         // && gameManagerScript.finishedTutorial
@@ -86,6 +103,9 @@ public class Card : MonoBehaviour
         set { _initialized = value; }
     }
 
+    /// <summary>
+    /// halts the cards when there is worng matching 
+    /// </summary>
     public void falseCheck()
     {
         StartCoroutine(pause());
@@ -101,61 +121,72 @@ public class Card : MonoBehaviour
     }
 
     private bool runned;
+    /// <summary>
+    /// This is called using the Unity on click functin. This function is called when the first card is clicked. 
+    /// </summary>
     public void firstCard()
     {
-        firstCardClicked = true;
-        GetComponent<Image>().sprite = _manager.GetComponent<GameManager>().getCardFace(_cardValue);
+        // stops the animation for the first card 
         animator.SetTrigger("OriginalCardState");
+        // gets the index of the matching card 
         int indexOfMatchingCard = gameManagerScript.getIndexOfMatchingFirstCard(_cardFace.name);
+        // if the function runs for first time then 
         if (!runned)
+            // display the animation for the worng matching card 
             gameManagerScript.playAnimationForWrongMatch();
         runned = true;
-
-
     }
 
-    
+    /// <summary>
+    /// This function is called form Unity function and increments the numOfClicksForFirstCard
+    /// </summary>
     public void setFirstCardClickedSecondTimeToTrue()
     {
         numOfClicksForFirstCard++;
         
         if (numOfClicksForFirstCard == 2)
+            // display the animation for the matching card
             gameManagerScript.playAnimationForCorrectgMatch();
     }
 
 
+    /// <summary>
+    /// This fucntion is called by all cards using Unity on click functionality 
+    /// </summary>
     public void stopAndPlayAnimation()
     {
-
-
-        if (gameManagerScript.cards[0].GetComponent<Image>().sprite.name == GetComponent<Image>().sprite.name)
+        
+        // if the sprite of the first card is the same as the sprite of the card which is being clicked AND the tutorial is not over then
+        if (gameManagerScript.cards[0].GetComponent<Image>().sprite.name == GetComponent<Image>().sprite.name && !gameManagerScript.finishedTutorial)
         {
+            // the tutorial is finished ( as this is the last stage which should be played after the wrong match card stage )
             gameManagerScript.finishedTutorial = true;
             sound.PlayOneShot(winning);
             animator.SetTrigger("OriginalCardState");
         }
 
+        // if the sprite of the first card is NOT the same as the sprite of the card which is being clicke AND tutorial is not over AND the number of matches is still half of all card ( which is 8 in this case )
+        // so if the number of matches redued then it means that the kid figured out how to play the game and we don't want this to run
         if ( (gameManagerScript.cards[0].GetComponent<Image>().sprite.name != GetComponent<Image>().sprite.name) && 
             (!gameManagerScript.finishedTutorial) && 
             (gameManagerScript._matches == gameManagerScript.cards.Length/2))
         {
             animator.SetTrigger("OriginalCardState");
             sound.PlayOneShot(losing);
-            gameManagerScript.playAnimationForOriginalCard();
+            gameManagerScript.playAnimationForFirstCard();
         }
 
-
     }
 
-
-    void playLosingSound()
+    /// <summary>
+    /// disalbe cards when state is 1 ( faced up ) and enables it otherwise ( when state is 0 ) 
+    /// </summary>
+    public void disableAndEnableCard()
     {
-        sound.PlayOneShot(losing);
-    }
-
-    void playWinningSound()
-    {
-        sound.PlayOneShot(winning);
+        if (_state == 1)
+            GetComponent<Button>().enabled = false;
+        else
+            GetComponent<Button>().enabled = true;
     }
 
     public Sprite getCardFace()
--- Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/GameManager.cs
+++ Assets/Scenes/WaitingRoom/MiniGames/SecondGame/Scripts/GameManager.cs
@@ -6,23 +6,37 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    // the list of the faces that are given for each card
     public Sprite[] cardFace;
+    // the face back, this is not a list as all cards have the same back sprite
     public Sprite cardBack;
     public GameObject[] cards;
+    // the text dispalyed in the bottom of the screen indicating the number of the matches that are left
     public Text matchText;
 
+    // boolean flag that checks if cards are initialised or not
     private bool _init = false;
+    // the number of matches left, 8 is assigned as there are 16 cards and there are 8 matches.
     public int _matches = 8;
 
+    // checks if the tutorial is finished or not
     public bool finishedTutorial;
 
+    // this gets index of the matching card for the first card
     public int indexForMatchingCard = 0;
 
+    /// <summary>
+    /// At the start of the game we want the animation for the first card to be displayed
+    /// </summary>
     void Start()
     {
         cards[0].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
     }
 
+    /// <summary>
+    /// this checks if the cards are intialised or not 
+    /// when the mouse is clicked then call the function checkCards
+    /// </summary>
     void Update()
     {
         if (!_init) { initializeCards(); }
@@ -30,6 +44,9 @@ public class GameManager : MonoBehaviour
             checkCards();
     }
 
+    /// <summary>
+    /// This initialises the cards so that for each card there is a matching. 
+    /// </summary>
     void initializeCards()
     {
         // every card has a match 
@@ -56,23 +73,36 @@ public class GameManager : MonoBehaviour
         if (!_init) _init = true;
     }
 
+    /// <summary>
+    /// returns the sprite for the card back    
+    /// </summary>
+    /// <returns></returns>
     public Sprite getCardBack()
     {
         return cardBack;
     }
 
+    /// <summary>
+    /// gets the index of the card that we want to get the face up and returns it.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
     public Sprite getCardFace(int i)
     {
         return cardFace[i - 1];
     }
 
+    /// <summary>
+    /// Checks if there is a match or not. 
+    /// </summary>
     void checkCards()
     {
 
         List<int> c = new List<int>();
+        // going through each card in the cards list 
         for (int i = 0; i < cards.Length; i++)
         {
-            // going through the list and if 1 card up then add to list
+            // and if the state is 1 ( card up ) then add to list
             if (cards[i].GetComponent<Card>().state == 1)
                 c.Add(i);
         }
@@ -82,30 +112,41 @@ public class GameManager : MonoBehaviour
             cardComparison(c);
     }
 
+    /// <summary>
+    /// compares if the cards that faces up have the same value or not. it takes a parameter c which is being passed from checkCards function 
+    /// </summary>
+    /// <param name="c"></param>
     void cardComparison(List<int> c)
     {
+        // this halt the game for few fractions of the second while the comparision takes place 
         Card.DO_NOT = true;
         int x = 0;
+        // from the list c in checkCards check if the value of the first card is the same as the value of the second card
         if (cards[c[0]].GetComponent<Card>().cardValue == cards[c[1]].GetComponent<Card>().cardValue)
         {
+            // if there is a match then disable the two matched cards
+            cards[c[0]].GetComponent<Button>().enabled = false;
+            cards[c[1]].GetComponent<Button>().enabled = false;
             x = 2;
             _matches--;
             matchText.text = "Number of Matches: " + _matches;
+            // if the number of matches is 0 then go to the menu scene 
             if (_matches == 0)
                 SceneManager.LoadScene("SecondGameMenu");
         }
+
+
         for (int i = 0; i < c.Count; i++)
-        { cards[c[i]].GetComponent<Card>().state = x;
+        {
+            cards[c[i]].GetComponent<Card>().state = x;
             cards[c[i]].GetComponent<Card>().falseCheck();
         }
     }
 
     
-
+    // gets the cardSpriteName for the first card then it goes through each card to check which card has the same sprite 
     public int getIndexOfMatchingFirstCard(string cardSpriteName)
     {
-        
-
         foreach (GameObject card in cards)
         {
             indexForMatchingCard++;
@@ -118,27 +159,40 @@ public class GameManager : MonoBehaviour
         return indexForMatchingCard;
     }
 
+    /// <summary>
+    /// plays the animation for the wrong matching card ( which is always one after the right matching card ) 
+    /// </summary>
     public void playAnimationForCorrectgMatch()
     {
         cards[indexForMatchingCard-1].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
     }
 
-    public void playAnimationForOriginalCard()
+    /// <summary>
+    /// plays the enlarge and the changing colour to yellow for the first card
+    /// </summary>
+    public void playAnimationForFirstCard()
     {
         cards[0].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
     }
 
+    /// <summary>
+    /// play animation for the wrong match
+    /// </summary>
     public void playAnimationForWrongMatch()
     {
+        // checks if the match is the last card then the worng matching card will not be one after it but rather it will be 1 before it 
         if (indexForMatchingCard == cards.Length-1 || indexForMatchingCard == cards.Length+1 || indexForMatchingCard == cards.Length)
         {
-            cards[indexForMatchingCard - 3].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
+            cards[indexForMatchingCard - 2].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
         }
+        // else the wrong matching card will be the one after the right matching card
         else 
         {
             cards[indexForMatchingCard].GetComponent<Animator>().SetTrigger("changeColourNEnlarge");
         }
         
     }
+
+
 
 }
