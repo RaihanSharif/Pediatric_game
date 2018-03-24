c181212379f25a426edcadb2e31e3eb88dd05b14
Alex <a.pessina@yandex.ru>
2018-03-24 19:23:06 +0000

    Update camera room tutorial with initial animation

---
+++ Assets/Scenes/CameraRoom/Scripts/SandBag2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandBag2 : MonoBehaviour {
+
+	public GameObject bag1;
+	public GameObject bag2;
+	public GameObject realBag1;
+	public GameObject realBag2;
+
+
+	void Start(){
+
+
+	}
+
+	
+	public void enableBoxColliders(){
+
+		realBag1.GetComponent<SpriteRenderer>().enabled = true;
+		realBag2.GetComponent<SpriteRenderer>().enabled = true;
+		bag1.SetActive(false);
+		bag2.SetActive(false);
+		
+
+
+
+
+	}
+}
