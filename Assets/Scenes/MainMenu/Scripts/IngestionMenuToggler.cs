a83d245bbc43c9706b42b138b2a23797ef6684ad
Alex <a.pessina@yandex.ru>
2018-03-25 14:53:27 +0100

    Fix food panel toggle in main menu again

---
+++ Assets/Scenes/MainMenu/Scripts/IngestionMenuToggler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngestionMenuToggler : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject ingestionMenu;
+
+	void Start(){
+		ingestionMenu.SetActive(false);
+	}
+
+	public void toggleIngestionMenu(){
+		ingestionMenu.SetActive(true);
+	}
+}
