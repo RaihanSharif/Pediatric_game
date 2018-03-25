e22c20720978cf05121dbfc584b78d1af16a989a
REKLV <rewaz96@hotmail.com>
2018-03-25 18:46:22 +0100

    added the tabalet and the cards
    The tablet is there but we will decide to add it later. But the deck of cards is working fine, when its being pressed the game starts.

---
+++ Assets/Scenes/WaitingRoom/Scripts/DeckOfCards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeckOfCards : MonoBehaviour {
+
+
+    public void OnMouseDown()
+    {
+        if (!PauseMenu.isPaused)
+        {
+            SceneManager.LoadScene("CardMatchingGameMenu");
+            WaitingRoomData.currentBarAmount -= RadialProgressBar.DECREASEBARAMOUNT;
+        }
+    }
+
+
+
+
+}
