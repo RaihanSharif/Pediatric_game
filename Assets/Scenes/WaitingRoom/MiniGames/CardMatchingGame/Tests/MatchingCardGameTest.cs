f8ac370f57eb0fa3d4545a0c935e28fd625c2f92
Daniel Lopes de Castro <danieldecastro98@hotmail.com>
2018-03-25 23:15:31 +0100

    Write initial tests
    Some of them pass

---
+++ Assets/Scenes/WaitingRoom/MiniGames/CardMatchingGame/Tests/MatchingCardGameTest.cs
@@ -0,0 +1,450 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MatchingCardGameTest {
+
+	private void LoadSceneByName(string name){
+		
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+
+	}
+
+//	List<int> comparison = new List<int>();
+//	comparison.Add(card1);
+//	comparison.Add(card2);
+//
+//	yield return null;
+//
+//	gmscript.cardComparison(comparison);
+
+	/// <summary>
+	/// Checks whether the first card stays up
+	/// if it matches the second card.
+	/// </summary>
+	/// <returns>If card1's state is 1.</returns>
+	[UnityTest]
+	public IEnumerator ValuesMatchCard1StateIsUp(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		yield return null;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual(1, card1script.state);
+
+	}
+
+	/// <summary>
+	/// Checks whether the second card stays up
+	/// if it matches the first card.
+	/// </summary>
+	/// <returns>If card2's state is 1.</returns>
+	[UnityTest]
+	public IEnumerator ValuesMatchCard2StateIsUp(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual(1, card2script.state);
+
+	}
+
+	/// <summary>
+	/// Checks whether the first card is disabled
+	/// if it matches the second card.
+	/// </summary>
+	/// <returns>If card1's enabled variable is false.</returns>
+	[UnityTest]
+	public IEnumerator ValuesMatchCard1IsDisabled(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual(false, card1script.enabled);
+
+	}
+
+	/// <summary>
+	/// Checks whether the second card is disabled
+	/// if it matches the first card.
+	/// </summary>
+	/// <returns>If card2's enabled variable is false.</returns>
+	[UnityTest]
+	public IEnumerator ValuesMatchCard2IsDisabled(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual(false, card2script.enabled);
+
+	}
+
+	/// <summary>
+	/// Checks whether the variable with the number of matches left
+	/// decrements when the two cards match.
+	/// </summary>
+	/// <returns>If the variable is equal to 7.</returns>
+	[UnityTest]
+	public IEnumerator CardsMatchCounterDecrements(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual(7, gmscript._matches);
+
+	}
+
+	/// <summary>
+	/// Checks whether the text that displays the number of matches left
+	/// updates when the two cards match.
+	/// </summary>
+	/// <returns>If the variable is equal to 7.</returns>
+	[UnityTest]
+	public IEnumerator CardsMatchTextUpdates(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 1;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		Assert.AreEqual("Number of Matches Left: 7", gmscript.matchText.text);
+
+	}
+
+
+	//=================================== WHEN CARD VALUES ARE DIFFERENT =====================================\\
+
+
+	/// <summary>
+	/// Checks whether the first card goes back down
+	/// if it doesn't match the second card.
+	/// </summary>
+	/// <returns>If card1's state is 0.</returns>
+	[UnityTest]
+	public IEnumerator ValuesDontMatchCard1StateIsDown(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual(0, card1script.state);
+
+	}
+
+	/// <summary>
+	/// Checks whether the second card goes back down
+	/// if it doesn't match the first card.
+	/// </summary>
+	/// <returns>If card2's state is 0.</returns>
+	[UnityTest]
+	public IEnumerator ValuesDontMatchCard2StateIsDown(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual(0, card2script.state);
+
+	}
+
+	/// <summary>
+	/// Checks whether the first card is enabled
+	/// if it doesn't match the second card.
+	/// </summary>
+	/// <returns>If card1's enabled variable is true.</returns>
+	[UnityTest]
+	public IEnumerator ValuesDontMatchCard1IsEnabled(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual(true, card1script.enabled);
+
+	}
+
+	/// <summary>
+	/// Checks whether the second card is enabled
+	/// if it doesn't match the first card.
+	/// </summary>
+	/// <returns>If card2's enabled variable is true.</returns>
+	[UnityTest]
+	public IEnumerator ValuesDontMatchCard2IsEnabled(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual(true, card2script.enabled);
+
+	}
+
+	/// <summary>
+	/// Checks whether the variable with the number of matches left
+	/// doesn't decrement when the two cards don't match.
+	/// </summary>
+	/// <returns>If the variable is still equal to 8.</returns>
+	[UnityTest]
+	public IEnumerator CardsDontMatchCounterDoesntDecrement(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual(8, gmscript._matches);
+
+	}
+
+	/// <summary>
+	/// Checks whether the text that displays the number of matches left
+	/// doesn't update when the two cards don't match.
+	/// </summary>
+	/// <returns>If the text is still the same after the comparison.</returns>
+	[UnityTest]
+	public IEnumerator CardsDontMatchTextDoesntUpdate(){
+
+		LoadSceneByName("CardMatchingGame");
+		yield return null;
+
+		var gm = GameObject.Find("GameManager");
+		var gmscript = gm.GetComponent<GameManager>();
+
+		yield return null;
+
+		var card1 = GameObject.Find("card 1");
+		var card2 = GameObject.Find("card 2");
+
+		var card1script = card1.GetComponent<Card>();
+		var card2script = card2.GetComponent<Card>();
+
+		card1script.cardValue = 1;
+		card2script.cardValue = 2;
+
+		card1.GetComponent<Button>().onClick.Invoke();
+		yield return null;
+
+		card2.GetComponent<Button>().onClick.Invoke();
+		yield return new WaitForSeconds(1);
+
+		Assert.AreEqual("Number of Matches Left: 8", gmscript.matchText.text);
+
+	}
+
+}
+	
