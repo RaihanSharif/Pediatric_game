b2f4f330668696f52a86f066d566c7d2a4549353
Amine Amor <amineamor@Amines-MacBook-Pro.local>
2018-03-26 16:12:40 +0100

    fix waiting room issues ( reload problem and success panel showing up too quickly )

--- Assets/Shared Scripts/LevelFinishedMenu.cs
+++ Assets/Shared Scripts/LevelFinishedMenu.cs
@@ -7,6 +7,7 @@ public class LevelFinishedMenu : MonoBehaviour {
 
 	//Flag to keep track if level is finished
 	public static bool isFinished = false;
+	private float counter = 0f;
 
 	//Reference to the menu panel that will have to be toggled 
 	[SerializeField]
@@ -19,6 +20,7 @@ public class LevelFinishedMenu : MonoBehaviour {
 	/// Pauses the game enables the level finished menu
  	/// </summary>
 	public void OnLevelFinished(){
+
 		levelFinishedMenuUI.SetActive(true);
 		isFinished = true;
 	}
@@ -27,13 +29,19 @@ public class LevelFinishedMenu : MonoBehaviour {
 	/// Loads the menu scene, defined in MENUSCENE constant and unfreezes game time
  	/// </summary>
 	public void Restart(){
+
 		Time.timeScale = 1f;
-		SceneManager.LoadScene(MENUSCENE);
+		SceneManager.LoadScene (MENUSCENE);
+		
+
+
 	}
 
 	public void LoadNextScene(string nextScene){
 		Time.timeScale = 1f;
 		SceneManager.LoadScene(nextScene);
+		WaitingRoomData.currentBarAmount = 100f;
+
 	}
 	
 
