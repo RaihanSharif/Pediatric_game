20e6660ae7131ea6dd145471254751251662cd5f
AmadNinja <amedeo.china@kcl.ac.uk>
2018-03-27 15:03:46 +0100

    Make base structure for System Testing

---
+++ Assets/SystemTest/SystemTest_IngestionBaby.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SystemTest_IngestionBaby {
+
+	private void LoadSceneByName(string name){
+
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+
+	}
+
+
+	[UnityTest]
+	[Timeout(100000000)]
+	public IEnumerator SystemTest_IngestionBabyPasses() {
+		LoadSceneByName ("SplashScreen");
+		yield return null;
+		yield return new WaitForSeconds (6.1f);
+		yield return null;
+
+		Assert.AreEqual (SceneManager.GetActiveScene ().name, "MainMenu");
+
+
+
+	}
+}
